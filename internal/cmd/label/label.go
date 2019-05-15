a42eac17b1d9c29e304f8d005307da029502ab45
Dimitar Petrov <buldozer9993@gmail.com>
2019-05-15 15:02:00 +0300

    Label command for resource labeling (#42)
    * Label command added
    
    * Fix formatting and command usage
    
    * Add tests
    
    * Add too much arguments validation
    
    * Make values passed by a --val flag

---
+++ internal/cmd/label/label.go
@@ -0,0 +1,110 @@
+package label
+
+import (
+	"fmt"
+	"github.com/Peripli/service-manager-cli/internal/cmd"
+	"github.com/Peripli/service-manager-cli/internal/output"
+	resperror "github.com/Peripli/service-manager-cli/pkg/errors"
+	"github.com/Peripli/service-manager-cli/pkg/types"
+	"github.com/Peripli/service-manager/pkg/query"
+	"github.com/Peripli/service-manager/pkg/web"
+	"github.com/spf13/cobra"
+)
+
+// Cmd wraps smctl label command
+type Cmd struct {
+	*cmd.Context
+
+	resourcePath string
+	id           string
+	values       []string
+	labelChanges types.LabelChanges
+}
+
+// NewLabelCmd returns new label command with context
+func NewLabelCmd(context *cmd.Context) *Cmd {
+	return &Cmd{Context: context, labelChanges: types.LabelChanges{}}
+}
+
+// Prepare returns cobra command
+func (c *Cmd) Prepare(prepare cmd.PrepareFunc) *cobra.Command {
+	result := &cobra.Command{
+		Use:   "label [resource] [id] [operation] [key] [--val value1 --val value2 ...]",
+		Short: "Label resource",
+		Long:  "Label resource",
+
+		PreRunE: prepare(c, c.Context),
+		RunE:    cmd.RunE(c),
+	}
+
+	result.Flags().StringArrayVar(&c.values, "val", []string{}, "Label value to be used")
+
+	return result
+}
+
+// Validate validates command's arguments
+func (c *Cmd) Validate(args []string) error {
+
+	resources := map[string]string{
+		"platform": web.PlatformsURL,
+		"broker":   web.ServiceBrokersURL,
+	}
+
+	operations := map[string]string{
+		"add":           "add",
+		"remove":        "remove",
+		"add-values":    "add_values",
+		"remove-values": "remove_values",
+	}
+
+	if len(args) < 4 {
+		return fmt.Errorf("resource type, id, operation, key and values are required")
+	}
+
+	if len(args) > 4 {
+		return fmt.Errorf("too much arguments, in case you have whitespaces in some of the arguments consider enclosig it with single quotes")
+	}
+
+	if v, ok := resources[args[0]]; ok {
+		c.resourcePath = v
+		c.id = args[1]
+	} else {
+		return fmt.Errorf("unknown resource")
+	}
+
+	labelChange := &query.LabelChange{}
+
+	if v, ok := operations[args[2]]; ok {
+		labelChange.Operation = query.LabelOperation(v)
+	} else {
+		return fmt.Errorf("unknown operation")
+	}
+
+	labelChange.Key = args[3]
+
+	if len(c.values) < 1 {
+		return fmt.Errorf("at least one value is required")
+	}
+
+	labelChange.Values = c.values
+	c.labelChanges.LabelChanges = append(c.labelChanges.LabelChanges, labelChange)
+	return nil
+}
+
+// Run runs the command's logic
+func (c *Cmd) Run() error {
+	err := c.Client.Label(c.resourcePath, c.id, &c.labelChanges)
+	if responseErr, ok := err.(resperror.ResponseError); ok {
+		return fmt.Errorf(responseErr.Description)
+	} else if err != nil {
+		return err
+	}
+
+	output.PrintMessage(c.Output, "Resource labeled successfully!")
+	return nil
+}
+
+// HideUsage hide command's usage
+func (c *Cmd) HideUsage() bool {
+	return true
+}
