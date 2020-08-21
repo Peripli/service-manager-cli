b6634af6b468bf137dbac81768094c7a30a2981c
thwiegan <wiegand.thomas@gmail.com>
2020-08-21 09:59:27 +0200

    Update update_broker.go

--- internal/cmd/broker/update_broker.go
+++ internal/cmd/broker/update_broker.go
@@ -101,7 +101,7 @@ func (ubc *UpdateBrokerCmd) Prepare(prepare cmd.PrepareFunc) *cobra.Command {
 		Short:   "Updates broker",
 		Long: `Update broker with name.
 Example:
-smctl update-broker broker '{"name": "new-name", "description": "new-description", "broker-url": "http://broker.com", "credentials": { "basic": { "username": "admin", "password": "admin" } }}'`,
+smctl update-broker broker '{"name": "new-name", "description": "new-description", "broker_url": "http://broker.com", "credentials": { "basic": { "username": "admin", "password": "admin" } }}'`,
 		PreRunE: prepare(ubc, ubc.Context),
 		RunE:    cmd.RunE(ubc),
 	}
