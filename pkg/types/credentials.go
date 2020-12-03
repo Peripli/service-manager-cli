c83a2ea9b7352d59f2d8e3378c85f4dadfaf0dec
pavelmaliy <pavel.maliy@sap.com>
2020-12-03 17:19:44 +0200

    add auth-type for platform registartion

--- pkg/types/credentials.go
+++ pkg/types/credentials.go
@@ -3,10 +3,17 @@ package types
 // Credentials contains types of credentials
 type Credentials struct {
 	Basic Basic `json:"basic,omitempty" yaml:"basic,omitempty"`
+	Oauth Oauth `json:"oauth,omitempty" yaml:"oauth,omitempty"`
 }
 
 // Basic wraps basic credentials
 type Basic struct {
 	User     string `json:"username,omitempty" yaml:"username,omitempty"`
 	Password string `json:"password,omitempty" yaml:"password,omitempty"`
+}
+
+// Oauth credentials
+type Oauth struct {
+	ClientID     string `json:"client_id,omitempty"`
+	ClientSecret string `json:"client_secret,omitempty"`
 }
