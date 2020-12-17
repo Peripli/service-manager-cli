2b077f13a3c8026b4aa5031f947a12d75aa4dc9c
EpsilonTal <33447266+EpsilonTal@users.noreply.github.com>
2020-12-17 15:45:04 +0200

    Updating test context with "purge"
    Co-authored-by: eyalnathan <eyal.nathan@sap.com>

--- internal/cmd/instance/deprovision_test.go
+++ internal/cmd/instance/deprovision_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Deprovision command test", func() {
 		})
 	})
 
-	Context("when force-delete parameter flag is used", func() {
+	Context("when purge flag is used", func() {
 		It("should pass it to SM", func() {
 			client.DeprovisionReturns("", nil)
 			promptBuffer.WriteString("y")
