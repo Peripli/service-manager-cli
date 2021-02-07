c9785d42d5b69eb8baf4d8137c4b10ca051c6d2e
i068165 <dmitry.bakaleinik@sap.com>
2021-02-07 12:02:37 +0200

    Improve test case readability

--- pkg/smclient/test/marketplace_test.go
+++ pkg/smclient/test/marketplace_test.go
@@ -69,81 +69,81 @@ var _ = Describe("Marketplace test", func() {
 			Description: "of5",
 			BrokerID:    "of5",
 		}
-		p1 := &types.ServicePlan{
-			ID:                "p1",
-			Name:              "p1",
-			Description:       "no metadata plan",
+		plan_supports_any_env := &types.ServicePlan{
+			ID:                "plan_supports_any_env",
+			Name:              "plan_supports_any_env",
+			Description:       "no metadata plan", // supports any env
 			ServiceOfferingID: "of1",
 		}
-		p2 := &types.ServicePlan{
-			ID:                "p2",
-			Name:              "p2",
+		plan_supports_cf_env1 := &types.ServicePlan{
+			ID:                "plan_supports_cf_env1",
+			Name:              "plan_supports_cf_env1",
 			Description:       "plan with cf support",
 			ServiceOfferingID: "of2",
 			Metadata:          []byte("{\"supportedPlatforms\":[\"cloudfoundry\"]}"),
 		}
-		p3 := &types.ServicePlan{
-			ID:                "p3",
-			Name:              "p3",
+		plan_supports_cf_env2 := &types.ServicePlan{
+			ID:                "plan_supports_cf_env2",
+			Name:              "plan_supports_cf_env2",
 			Description:       "plan with cf support",
 			ServiceOfferingID: "of3",
 			Metadata:          []byte("{\"supportedPlatforms\":[\"cloudfoundry\"]}"),
 		}
-		p4 := &types.ServicePlan{
-			ID:                "p4",
-			Name:              "p4",
+		plan_supports_k8s := &types.ServicePlan{
+			ID:                "plan_supports_k8s",
+			Name:              "plan_supports_k8s",
 			Description:       "plan with k8s support",
 			ServiceOfferingID: "of3",
 			Metadata:          []byte("{\"supportedPlatforms\":[\"kubernetes\"]}"),
 		}
-		p5 := &types.ServicePlan{
-			ID:                "p5",
-			Name:              "p5",
+		plan_supports_k8s_and_cf := &types.ServicePlan{
+			ID:                "plan_supports_k8s_and_cf",
+			Name:              "plan_supports_k8s_and_cf",
 			Description:       "plan with cf & k8s support",
 			ServiceOfferingID: "of4",
 			Metadata:          []byte("{\"supportedPlatforms\":[\"kubernetes\",\"cloudfoundry\"]}"),
 		}
-		p6 := &types.ServicePlan{
-			ID:                "p6",
-			Name:              "p6",
+		plan_supports_test_env := &types.ServicePlan{
+			ID:                "plan_supports_test_env",
+			Name:              "plan_supports_test_env",
 			Description:       "plan with test env support",
 			ServiceOfferingID: "of5",
 			Metadata:          []byte("{\"supportedPlatforms\":[\"test-env\"]}"),
 		}
-		rof1 := &types.ServiceOffering{
+		rof1_with_plan_any_env := &types.ServiceOffering{
 			ID:          "of1",
 			Name:        "of1",
 			Description: "of1",
 			BrokerID:    "of1",
-			Plans:       []types.ServicePlan{*p1},
+			Plans:       []types.ServicePlan{*plan_supports_any_env},
 		}
-		rof2 := &types.ServiceOffering{
+		rof2_with_plan_cf_env := &types.ServiceOffering{
 			ID:          "of2",
 			Name:        "of2",
 			Description: "of2",
 			BrokerID:    "of2",
-			Plans:       []types.ServicePlan{*p2},
+			Plans:       []types.ServicePlan{*plan_supports_cf_env1},
 		}
-		rof3 := &types.ServiceOffering{
+		rof3_with_plan_cf_and_k8s := &types.ServiceOffering{
 			ID:          "of3",
 			Name:        "of3",
 			Description: "of3",
 			BrokerID:    "of3",
-			Plans:       []types.ServicePlan{*p3, *p4},
+			Plans:       []types.ServicePlan{*plan_supports_cf_env2, *plan_supports_k8s},
 		}
-		rof4 := &types.ServiceOffering{
+		rof4_with_plan_cf_and_k8s := &types.ServiceOffering{
 			ID:          "of4",
 			Name:        "of4",
 			Description: "of4",
 			BrokerID:    "of4",
-			Plans:       []types.ServicePlan{*p5},
+			Plans:       []types.ServicePlan{*plan_supports_k8s_and_cf},
 		}
-		rof5 := &types.ServiceOffering{
+		rof5_with_plan_test_env := &types.ServiceOffering{
 			ID:          "of5",
 			Name:        "of5",
 			Description: "of5",
 			BrokerID:    "of5",
-			Plans:       []types.ServicePlan{*p6},
+			Plans:       []types.ServicePlan{*plan_supports_test_env},
 		}
 
 		BeforeEach(func() {
@@ -168,31 +168,31 @@ var _ = Describe("Marketplace test", func() {
 					ResponseBodyProvider: func(req *http.Request) []byte {
 						if strings.Contains(req.RequestURI, "service_offering_id+eq+%27of1%27") {
 							response, _ := json.Marshal(types.ServicePlans{
-								ServicePlans: []types.ServicePlan{*p1},
+								ServicePlans: []types.ServicePlan{*plan_supports_any_env},
 							})
 							return response
 						}
 						if strings.Contains(req.RequestURI, "service_offering_id+eq+%27of2%27") {
 							response, _ := json.Marshal(types.ServicePlans{
-								ServicePlans: []types.ServicePlan{*p2},
+								ServicePlans: []types.ServicePlan{*plan_supports_cf_env1},
 							})
 							return response
 						}
 						if strings.Contains(req.RequestURI, "service_offering_id+eq+%27of3%27") {
 							response, _ := json.Marshal(types.ServicePlans{
-								ServicePlans: []types.ServicePlan{*p3, *p4},
+								ServicePlans: []types.ServicePlan{*plan_supports_cf_env2, *plan_supports_k8s},
 							})
 							return response
 						}
 						if strings.Contains(req.RequestURI, "service_offering_id+eq+%27of4%27") {
 							response, _ := json.Marshal(types.ServicePlans{
-								ServicePlans: []types.ServicePlan{*p5},
+								ServicePlans: []types.ServicePlan{*plan_supports_k8s_and_cf},
 							})
 							return response
 						}
 						if strings.Contains(req.RequestURI, "service_offering_id+eq+%27of5%27") {
 							response, _ := json.Marshal(types.ServicePlans{
-								ServicePlans: []types.ServicePlan{*p6},
+								ServicePlans: []types.ServicePlan{*plan_supports_test_env},
 							})
 							return response
 						}
@@ -208,11 +208,11 @@ var _ = Describe("Marketplace test", func() {
 			})
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(result.ServiceOfferings).To(HaveLen(5))
-			Expect(result.ServiceOfferings[0]).To(Equal(*rof1))
-			Expect(result.ServiceOfferings[1]).To(Equal(*rof2))
-			Expect(result.ServiceOfferings[2]).To(Equal(*rof3))
-			Expect(result.ServiceOfferings[3]).To(Equal(*rof4))
-			Expect(result.ServiceOfferings[4]).To(Equal(*rof5))
+			Expect(result.ServiceOfferings[0]).To(Equal(*rof1_with_plan_any_env))
+			Expect(result.ServiceOfferings[1]).To(Equal(*rof2_with_plan_cf_env))
+			Expect(result.ServiceOfferings[2]).To(Equal(*rof3_with_plan_cf_and_k8s))
+			Expect(result.ServiceOfferings[3]).To(Equal(*rof4_with_plan_cf_and_k8s))
+			Expect(result.ServiceOfferings[4]).To(Equal(*rof5_with_plan_test_env))
 		})
 		It("should return cloudfoundry services with all plans if cloudfoundry environment requested", func() {
 			result, err := client.Marketplace(&cliquery.Parameters{
@@ -221,10 +221,10 @@ var _ = Describe("Marketplace test", func() {
 			})
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(result.ServiceOfferings).To(HaveLen(4))
-			Expect(result.ServiceOfferings[0]).To(Equal(*rof1)) // matches as it any
-			Expect(result.ServiceOfferings[1]).To(Equal(*rof2))
-			Expect(result.ServiceOfferings[2]).To(Equal(*rof3))
-			Expect(result.ServiceOfferings[3]).To(Equal(*rof4))
+			Expect(result.ServiceOfferings[0]).To(Equal(*rof1_with_plan_any_env)) // matches as it any
+			Expect(result.ServiceOfferings[1]).To(Equal(*rof2_with_plan_cf_env))
+			Expect(result.ServiceOfferings[2]).To(Equal(*rof3_with_plan_cf_and_k8s))
+			Expect(result.ServiceOfferings[3]).To(Equal(*rof4_with_plan_cf_and_k8s))
 		})
 		It("should return kubernetes services with all plans if kubernetes environment requested", func() {
 			result, err := client.Marketplace(&cliquery.Parameters{
@@ -233,9 +233,9 @@ var _ = Describe("Marketplace test", func() {
 			})
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(result.ServiceOfferings).To(HaveLen(3))
-			Expect(result.ServiceOfferings[0]).To(Equal(*rof1)) // matches as it any
-			Expect(result.ServiceOfferings[1]).To(Equal(*rof3))
-			Expect(result.ServiceOfferings[2]).To(Equal(*rof4))
+			Expect(result.ServiceOfferings[0]).To(Equal(*rof1_with_plan_any_env)) // matches as it any
+			Expect(result.ServiceOfferings[1]).To(Equal(*rof3_with_plan_cf_and_k8s))
+			Expect(result.ServiceOfferings[2]).To(Equal(*rof4_with_plan_cf_and_k8s))
 		})
 		It("should return test-env services with all plans if test-env environment requested", func() {
 			result, err := client.Marketplace(&cliquery.Parameters{
@@ -244,8 +244,8 @@ var _ = Describe("Marketplace test", func() {
 			})
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(result.ServiceOfferings).To(HaveLen(2))
-			Expect(result.ServiceOfferings[0]).To(Equal(*rof1)) // matches as it any
-			Expect(result.ServiceOfferings[1]).To(Equal(*rof5))
+			Expect(result.ServiceOfferings[0]).To(Equal(*rof1_with_plan_any_env)) // matches as it any
+			Expect(result.ServiceOfferings[1]).To(Equal(*rof5_with_plan_test_env))
 		})
 	})
 
